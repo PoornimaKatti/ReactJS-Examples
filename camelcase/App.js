2b30b37102cd19606f116680a5fee11c66323d42
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-03-04 20:49:15 +0530

    Add files via upload

---
+++ camelcase/App.js
@@ -0,0 +1,14 @@
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+  
+class App extends React.Component {  
+  render() {  
+    return (  
+      <div>  
+      <h1 style={{color: "Red"}}>Hello!</h1>  
+      <p style={{backgroundColor: "lightgreen"}}>Positive thinking overwhelms stress and it will allow you to get rid of stress.</p>  
+      </div>  
+    );  
+  }  
+}  
+export default App          
