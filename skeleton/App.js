0ba958fc26c303018595df81711f11eb247ed491
PoornimaKatti <153407034+PoornimaKatti@users.noreply.github.com>
2024-03-14 15:05:43 +0530

    Add files via upload

---
+++ skeleton/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GradientBackground from './GradientBackground'
+
+function App() {
+  return (
+    <GradientBackground>
+      <h1>Hello, React Gradient Background!</h1>
+    </GradientBackground>
+  );
+}
+
+export default App;
---
+++ skeleton/GradientBackground.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const GradientBackground = styled.div`
+  background: linear-gradient(to bottom, #ff7e5f, #feb47b);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 24px;
+`;
+
+export default GradientBackground
